78eddf0ab98bd8be3b71a293462168ca61ae3b38
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-04-26 17:30:32 +0200

    update

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT=process.env.PORT  || 3000; 
+export const PORT=process.env.PORT  || 3000;
 export const DOMAIN= process.env.DOMAIN || "http://localhost"
 
 export const DB_USER= process.env.DB_USER ||"jaumeesquerdo"
