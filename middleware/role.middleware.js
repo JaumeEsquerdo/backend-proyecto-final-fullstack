7cad4c109972f401935e5eef5479f44bd692e18d
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-04-26 18:01:29 +0200

    Update role.middleware.js

--- middleware/role.middleware.js
+++ middleware/role.middleware.js
@@ -1,10 +1,10 @@
-export const isAdmin = (req, res, next) =>{
+export const isAdmin = (req, res, next) => {
     //verificar si el usuario tiene role de admin
-    if(req.user && req.user.role === 'admin'){
+    if (req.user && req.user.role === 'admin') {
         return next(); //continuar si es admin
-    }else{
+    } else {
         return res.status(403).json({
-            msg:'Acceso denegado, solo admins'
+            msg: 'Acceso denegado, solo admins'
         })
     }
 }
